8e290e14479a9148f920aec9a9fb1032bb2aca73
INW Manufacturing <52832346+INWMfg@users.noreply.github.com>
2021-03-01 18:01:09 -0700

    Initial Upload
    Additional Error Handling and re-connection abilities coming soon.

---
+++ net5Demo/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Management;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO.Ports; 
+using System.Threading;
+
+namespace net5Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProxCardReader reader = new ProxCardReader();
+            reader.CardReceived += Reader_CardReceived;
+            reader.InitWatcher();
+
+            TimerCallback callback = new TimerCallback(Tick);
+            _timer = new Timer(callback, null, new TimeSpan(0,0,0), new TimeSpan(0,0,1));
+
+            Console.ReadLine();
+        }
+
+        private static void Reader_CardReceived(object sender, CardDataEventArgs e)
+        {
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            sb.AppendLine($"Facility: {e.FacilityId}");
+            sb.AppendLine($"Card:     {e.CardId}");
+            sb.AppendLine($"{DateTime.Now}");
+
+            Console.WriteLine(sb.ToString());
+        }
+
+        private static int Counter = 0;
+        private static System.Threading.Timer _timer;
+        private static void Tick(Object o) => Console.WriteLine(Counter++);
+        
+
+    }
+
+
+}
+
+
+
+            //List<string> portnames = SerialPort.GetPortNames().ToList();
+
+
+            /*
+            using (var searcher = new ManagementObjectSearcher
+                ("SELECT * FROM WIN32_SerialPort"))
+            {
+                string[] portnames = SerialPort.GetPortNames();
+                List<ManagementBaseObject> ports = searcher.Get().Cast<ManagementBaseObject>().ToList();
+                ports.ForEach(p => Console.WriteLine($"{p.ToString()}"));
+
+                var tList = (from n in portnames
+                            join p in ports on n equals p["DeviceID"].ToString()
+                            select n + " - " + p["Caption"]).ToList();
+
+                tList.ForEach(Console.WriteLine);
+            }
+
+            // pause program execution to review results...
+            Console.WriteLine("Press enter to exit");
+            Console.ReadLine();
+            */
+
+            //Win32_USBHub - multiple listed. the one i want isnt listed. 
+            //Win32_SerialPort - 3 devices. the one i want isnt listed. 
+            //Win32_USBControllerDevice - no results
+            //Win32_PnPEntity
+
+    /*
+            string vid = "1A86";
+            string pid = "7523";
+
+
+            ManagementScope connectionScope = new ManagementScope();
+            SelectQuery serialQuery = new SelectQuery($@"SELECT * FROM Win32_PnPEntity where DeviceID like '%VID_{vid}&PID_{pid}%'");
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(connectionScope, serialQuery);
+
+            int count = 0;
+            foreach (ManagementObject item in searcher.Get())
+            try
+            {
+                count++;
+               // string descr = item.ToString();
+
+                //if (descr.Contains("1A86"))
+                //    Console.WriteLine(descr);
+
+
+                    string desc = "";//item["Description"].ToString();
+                    string deviceId = item["DeviceID"].ToString();
+                    
+                    if (desc.Contains("1A86") || deviceId.Contains("1A86"))
+                        Console.WriteLine($"{deviceId} {desc}");
+
+
+
+                    //if (desc.Contains("Arduino"))
+                    //{
+                    //    return deviceId;
+                    //}
+               
+            }
+            catch (ManagementException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine($"{count} devices found");
+*/
---
+++ net5Demo/ProxCardReader.cs
@@ -0,0 +1,386 @@
+using System;
+using System.Management;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.IO;
+
+
+namespace net5Demo
+{
+    public class ProxCardReader
+    {
+        public UInt16 Vid { get; set; } = 0x1A86;
+        public UInt16 Pid { get; set; } = 0x7523;
+        public int DeviceCount {get; private set; } = 0;
+        public string ComPort { get; private set; } = string.Empty;
+        public string DeviceId { get; private set; } = string.Empty;
+        public string DeviceName { get; private set; } = string.Empty;
+        public DateTime LastReceived { get; private set; }
+
+        #region EventHandlers
+        public delegate void CardReceivedEvent(object sender, CardDataEventArgs e);
+        public event CardReceivedEvent CardReceived = null;
+        protected virtual void OnCardReceived(CardDataEventArgs args) => CardReceived?.Invoke(this, args);
+        #endregion
+        #region watcher
+        private System.Threading.Timer watcherTimer;
+        public void InitWatcher() => InitWatcher(new TimeSpan(0,0,5));
+        public void InitWatcher(TimeSpan interval)
+        {
+            TimerCallback callback = new TimerCallback(Search);
+            watcherTimer = new Timer(callback, null, new TimeSpan(0,0,0), interval);
+        }
+        private void Search(Object o)
+        {           
+            ManagementScope connectionScope = new ManagementScope();
+            SelectQuery mosQuery = new SelectQuery($@"SELECT * FROM Win32_PnPEntity where DeviceID like '%VID_{Vid.ToString("X4")}&PID_{Pid.ToString("X4")}%'");
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(connectionScope, mosQuery);
+            ManagementObjectCollection results = searcher.Get();
+            ManagementObjectCollection.ManagementObjectEnumerator enumerator = results.GetEnumerator();
+
+            DeviceCount = results.Count;
+            if (DeviceCount > 0)
+            {
+                enumerator.MoveNext();
+                InitDevice((ManagementObject)enumerator.Current);
+            }
+                
+
+        }
+        private void InitDevice(ManagementObject device)
+        {
+            try
+            {
+                if (!string.IsNullOrWhiteSpace(DeviceId))
+                    return;
+
+                DeviceId = device["DeviceID"].ToString();
+                DeviceName = device["Name"].ToString();                
+                /* Extract the ComPort using a regular expression
+                \(          # Escaped parenthesis, means "starts with a '(' character"
+                    (       # Parentheses in a regex mean "put (capture) the stuff in between into the Groups array
+                    [^)]    # Any character that is not a ')' character
+                    *       # Zero or more occurrences of the aforementioned "non ')' char"
+                    )       # Close the capturing group
+                \)          # "Ends with a ')' character"
+                */
+                ComPort = Regex.Match(DeviceName, @"\(([^)]*)\)", RegexOptions.IgnoreCase).Groups[1].Value;                
+                InitReader();                
+            }
+            catch(System.Exception ex)
+            {
+                DeviceId = string.Empty;
+                DeviceName = string.Empty;
+            }
+        }
+        #endregion
+        #region Device
+        private System.IO.Ports.SerialPort serialPort;
+        private bool IsReading = false;
+        private List<byte> InputBuffer = null;
+
+        
+               
+        private void InitReader()
+        {
+            try
+            {
+                serialPort = new System.IO.Ports.SerialPort();
+                serialPort.PortName = ComPort;
+                
+                serialPort.BaudRate = 9600;
+                serialPort.Parity = System.IO.Ports.Parity.None;
+                serialPort.StopBits = System.IO.Ports.StopBits.One;
+                serialPort.Handshake = System.IO.Ports.Handshake.None; 
+                serialPort.DataBits = 8;        
+                serialPort.Open();
+
+                ReadData();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+        private async void ReadData()
+        {
+            try
+            {
+                if (serialPort == null)
+                    return;
+
+                if (IsReading)
+                    return;
+
+                if (InputBuffer == null)
+                    InputBuffer = new List<byte>();
+
+                IsReading = true;
+                LastReceived = DateTime.Now;
+
+                while (true)
+                {
+
+                    
+                    int b = serialPort.ReadByte();
+                    if (b == 6)
+                    {
+                        // Do Nothing - Unit is jusr responding to a keep alive request.
+                    }
+                    else if (b == 2)
+                    {
+                        InputBuffer.Clear();
+                        bool ContinueRead = true;
+                        while (ContinueRead)
+                        {
+                            //bitCount = await reader.LoadAsync(1); //.AsTask();
+                            b = serialPort.ReadByte();
+
+                            if (System.Diagnostics.Debugger.IsAttached)
+                                System.Diagnostics.Debug.Write(b);
+
+                            if (b != 3)
+                                InputBuffer.Add((byte)b);
+                            else
+                                ContinueRead = false;
+                        }                       
+                        string result = System.Text.Encoding.UTF8.GetString(InputBuffer.ToArray());
+                        CardDataEventArgs args = new CardDataEventArgs(result);
+                        OnCardReceived(args);
+                    }
+                    else
+                    {
+                        //bitCount = await reader.LoadAsync(1); //.AsTask();
+
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                IsReading = false;
+            }
+        }
+
+        #endregion
+        /*
+
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Devices.SerialCommunication;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+        public UInt16 Vid { get; set; } = 0x1A86;
+        public UInt16 Pid { get; set; } = 0x7523;
+        public DateTime LastReceived { get; private set; }
+
+        #region EventHandlers
+        public delegate void CardReceivedEvent(object sender, CardDataEventArgs e);
+        public event CardReceivedEvent CardReceived = null;
+        protected virtual void OnCardReceived(CardDataEventArgs args) => CardReceived?.Invoke(this, args);
+        #endregion
+
+        #region DeviceWatcher
+        private DeviceWatcher watcher = null;
+        public void InitWatcher()
+        {
+            string devSelector = SerialDevice.GetDeviceSelectorFromUsbVidPid(Vid, Pid);
+            watcher = DeviceInformation.CreateWatcher(devSelector);
+            watcher.Added += Watcher_Added;
+            watcher.Removed += Watcher_Removed;
+            watcher.EnumerationCompleted += Watcher_EnumerationCompleted;
+            watcher.Start();
+
+            InitTimer();
+        }
+        private async void Watcher_Removed(DeviceWatcher sender, DeviceInformationUpdate args)
+        {
+            //throw new NotImplementedException();
+        }
+
+        private async void Watcher_Added(DeviceWatcher sender, DeviceInformation args)
+        {
+            try
+            {
+                devInfo = args;
+                device = await SerialDevice.FromIdAsync(devInfo.Id);
+
+                if (device != null)
+                    InitDevice();
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+        }
+        private async void Watcher_EnumerationCompleted(DeviceWatcher sender, object args)
+        {
+            //throw new NotImplementedException();
+        }
+
+
+
+
+
+        #endregion
+
+        #region Device
+        private DeviceInformation devInfo = null;
+        private SerialDevice device = null;
+        public Boolean IsDeviceConnected => (device != null);
+
+        public void InitDevice()
+        {
+            try
+            {
+                if (IsDeviceConnected)
+                {
+                    device.BaudRate = 9600;
+                    device.Parity = SerialParity.None;
+                    device.StopBits = SerialStopBitCount.One;
+                    device.Handshake = SerialHandshake.None;
+                    device.DataBits = 8;
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+        #endregion
+
+        #region DataReader
+        private DataReader reader = null;
+        private List<byte> InputBuffer = null;
+        private void InitReader()
+        {
+            try
+            {
+                reader = new DataReader(device.InputStream);
+                reader.InputStreamOptions = InputStreamOptions.Partial;
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private bool IsReading = false;
+
+        private async void ReadData()
+        {
+            try
+            {
+                if (device == null)
+                    return;
+
+                if (reader == null)
+                    InitReader();
+
+                if (IsReading)
+                    return;
+
+                if (InputBuffer == null)
+                    InputBuffer = new List<byte>();
+
+                IsReading = true;
+
+                reader.InputStreamOptions = InputStreamOptions.Partial;
+
+                //reader.loa
+                uint bitCount = await reader.LoadAsync(1); //.AsTask();
+                LastReceived = DateTime.Now;
+
+                while (true)
+                {
+                    byte b = reader.ReadByte();
+                    if (b == 6)
+                    {
+                        // Do Nothing - Unit is jusr responding to a keep alive request.
+                    }
+                    else if (b == 2)
+                    {
+                        InputBuffer.Clear();
+                        bool ContinueRead = true;
+                        while (ContinueRead)
+                        {
+                            bitCount = await reader.LoadAsync(1); //.AsTask();
+                            b = reader.ReadByte();
+
+                            if (System.Diagnostics.Debugger.IsAttached)
+                                System.Diagnostics.Debug.Write(b);
+
+                            if (b != 3)
+                                InputBuffer.Add(b);
+                            else
+                                ContinueRead = false;
+                        }                       
+                        string result = System.Text.Encoding.UTF8.GetString(InputBuffer.ToArray());
+                        CardDataEventArgs args = new CardDataEventArgs(result);
+                        OnCardReceived(args);
+                    }
+                    else
+                    {
+                        bitCount = await reader.LoadAsync(1); //.AsTask();
+
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                IsReading = false;
+            }
+        }
+        #endregion
+
+        #region Timer
+        private DispatcherTimer readtimer = null;
+        private void InitTimer()
+        {
+            try
+            {
+                readtimer = new DispatcherTimer();
+                readtimer.Tick += ReadTimer_Tick;
+                readtimer.Interval = new TimeSpan(0, 0, 0, 0, 100);
+                readtimer.Start();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+        private void ReadTimer_Tick(object sender, object e)
+        {
+            try
+            {
+                ReadData();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }       
+        #endregion
+
+
+*/
+    }
+}
